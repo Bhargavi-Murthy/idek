f3180d857baa4b54af55b6c3956993cb2cc861ab
Bhargavi-Murthy <115211115+Bhargavi-Murthy@users.noreply.github.com>
2024-06-07 11:00:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ model = genai.GenerativeModel("gemini-pro")
 
 # Initialize Streamlit app
 st.set_page_config(page_title="POOPOO LAND")
-st.header("solely to be used by people titled poopoo")
+st.header("This website is for educational purposes only")
 
 input_text = st.text_area("Input: ", "type in things you want to know about.")
 submit = st.button("Explain this to a five-year-old")
